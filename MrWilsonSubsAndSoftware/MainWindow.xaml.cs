39714e7d072f484ff299d76f74027c0f6af63c11
Mr. Wilson <bwilson@macon.k12.mo.us>
2020-09-14 14:54:26 -0500

    third Commit

--- MrWilsonSubsAndSoftware/MainWindow.xaml.cs
+++ MrWilsonSubsAndSoftware/MainWindow.xaml.cs
@@ -23,6 +23,82 @@ namespace MrWilsonSubsAndSoftware
         public MainWindow()
         {
             InitializeComponent();
+
+            headingtextBlock.Text = "Welcome to Mr. Wilson's Subs and Software!  Please order bellow." +
+                "              *** Sorry at this time you can only order one sub at a time. We need a better web site.";
+        }
+
+        private void orderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            // Variables
+
+            string subDiscription =" Six Inch ";
+
+            double subTotalPrice = 3.50;
+
+
+            // Get Size
+
+            if (footLongRadioButton3.IsChecked == true)
+            {
+                subTotalPrice = 5.00;
+
+                subDiscription = "Foot Long ";
+            }
+
+
+
+            // Get Bread
+
+            if (wheatradioButton.IsChecked == true)
+            {
+                subDiscription = subDiscription + " Wheat bread "; 
+            }
+            else if (italianRadioButton1.IsChecked== true)
+            {
+                subDiscription = subDiscription + " Italian bread ";
+            }
+            else
+            {
+                subDiscription = subDiscription + " Flat bread ";
+
+                subTotalPrice = subTotalPrice + .50;
+            }
+
+            //Get Meat
+            if (hamradioButton.IsChecked == true)
+            {
+                subDiscription = subDiscription + " Ham , ";
+            }
+            else if (ChickenradioButton3.IsChecked == true)
+            {
+                subDiscription = subDiscription + " Roasted Chicken, ";
+            }
+            else if (MeatBallradioButton4.IsChecked == true)
+            {
+                subDiscription = subDiscription + " Meatball, ";
+
+                subTotalPrice = subTotalPrice + 1.00;
+
+            }
+            else
+            {
+                subDiscription = subDiscription + " Steak, ";
+
+                subTotalPrice = subTotalPrice + 1.50;
+            }
+
+
+            //Get Cheese
+
+            //Get Veggies
+
+            // Check for Special
+
+            //Print - Sub and Total Price
+
         }
     }
 }
